fc79100df79acf0da70efe54b8737dfa4cd63b00
cesar.aguilar <Blueavian9@gmail.com>
2022-05-11 11:35:02 -0700

    update work

--- src/index.js
+++ src/index.js
@@ -2,3 +2,36 @@ import './less/index.less'
 
 // Your code goes here!
 console.log("Hello World!");
+const universalSelector = document.querySelector("*");
+
+// 1 - load Event
+    window.onload = function (evt) {
+        console.log(`event ${evt.type} fired! Ready togo!`)
+        const heading = document.querySelector('h1')
+        heading.textContent = 'READY TO GO!!'
+    }
+
+  // 2 - copy Event
+    window.addEventListener('copy', () => {
+        //universalSelector.clipboard.readText();
+        console.log('pass');
+        // eslint-disable-next-line no-undef
+    })
+  
+
+// 3 - click
+    document.body.addEventListener('click', evt => {
+      evt.target.classList.toggle('mirror')  
+    })
+
+// 4 - dblclick
+    document.body.addEventListener('dblclick', evt => {
+      evt.target.outerHTML = ''
+    })
+
+// 5 - keyword
+    window.addEventListener('keydown', evt => {
+      if (evt.key === 6) {
+          document.body.innerHTML = 'LORD VADER EXECUTE ORDER 66'
+      }
+  })
