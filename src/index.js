97215b0ae0822a2c2d9f603374563caa79ab25d2
cesar.aguilar <Blueavian9@gmail.com>
2022-05-17 18:54:56 -0700

    final push

--- src/index.js
+++ src/index.js
@@ -1,53 +1,101 @@
 import './less/index.less'
 
 // Your code goes here!
-console.log("Hello World!");
-// eslint-disable-next-line no-unused-vars
-const universalSelector = document.querySelector("*");
 
-// 1 - load Event
-    window.onload = function (evt) {
-        console.log(`event ${evt.type} fired! Ready togo!`)
-        const heading = document.querySelector('h1')
-        heading.textContent = 'READY TO GO!!'
-    }
-    console.log(onload);
-
-  // 2 - copy Event
-    window.addEventListener('copy', () => {
-        //universalSelector.clipboard.readText();
-        console.log('pass');
-        // eslint-disable-next-line no-undef
-    })  
-
-// 3 - click
-    document.body.addEventListener('click', evt => {
-      evt.target.classList.toggle('mirror')  
+// assigning a variable to the nav-link
+const navBarAnchor = document.querySelectorAll(".nav-link");
+// mouse over = blue
+navBarAnchor.forEach((element) => {
+    element.addEventListener("mouseover", (event) => {
+        event.target.style.color = "blue";
     })
-
-// 4 - dblclick
-    document.body.addEventListener('dblclick', evt => {
-      evt.target.outerHTML = ''
+});
+// mouse out = black
+navBarAnchor.forEach((element) => {
+    element.addEventListener("mouseout", (event) => {
+        event.target.style.color = "black";
     })
+});
 
-// 5 - keyword
-    window.addEventListener('keydown', evt => {
-      if (evt.key === 6) {
-          document.body.innerHTML = 'LORD VADER EXECUTE ORDER 66'
-      }
-  })
+// KEYDOWN
+const colorChangeP = document.querySelectorAll("p");
+colorChangeP.forEach(p => {
+    p.setAttribute("tabindex", "1");
+    p.addEventListener("keydown", event => {
+        event.target.style.fontFamily = "Courier New,Courier","Lucida Sans Typewriter","Lucida Typewriter,monospace";
+    })
+});
 
-// 6 - MouseOver
-  document.body.addEventListener('mouseover', evt => {
-      
-  })
+// WHEEL
+document.querySelectorAll("img").forEach(img => {
+    img.addEventListener("wheel", event => {
+        event.target.style.transform = "scaleX(-1)";
+    })
+});
 
-// 7 - Focus 
+// LOAD
+window.addEventListener("load", () => {
+    alert("💻Finish Loading💻");
+});
 
-// 8 scroll
+// Working on it!
+//console.log(document.querySelector(".btn"));
+document.querySelector(".btn").addEventListener("focus", () => {
+});
 
-// 9 - select 
+window.addEventListener("resize", () => {
+    let divBtn = document.querySelectorAll(".btn");
+    divBtn.forEach(btn => {
+        btn.style.backgroundColor = "#DEB887";
+    })
+})
 
-// 10 - drag / drop
+window.addEventListener("scroll", () => {
+    document.querySelector("header").style.position = "sticky";
+    document.querySelector("header").style.top = "0";
+})
 
+document.querySelector("h1").addEventListener("dblclick", (event) => {
+    event.target.textContent = "You Clicked On Me!";
+})
 
+document.querySelectorAll(".btn").forEach(btn => {
+    btn.addEventListener("click", event => {
+        console.log("Prevent Default: ", event);
+        event.preventDefault();
+    })
+})
+
+// create a input element
+const forFocus = document.createElement("input");
+forFocus.setAttribute("type", "text");
+forFocus.setAttribute("value", "select me!..select me!");
+document.querySelector("header div").appendChild(forFocus);
+// focus the input element
+document.querySelector("header input").addEventListener("focus", event => {
+    event.target.style.backgroundColor = "#00BFFF";
+})
+
+// select a <input>
+forFocus.addEventListener("select", () => {
+    document.querySelectorAll("h2").forEach(h2 => {
+        h2.style["-webkit-text-stroke"] = "1px black";
+        h2.style["-webkit-text-fill-color"] = "white";
+    })
+})
+
+//drag img
+let dragItem = document.getElementsByClassName("img-fluid rounded")[0];
+dragItem.setAttribute("draggable", "true");
+dragItem.style.cursor = "pointer";
+
+dragItem.addEventListener("dragstart", () => {
+    setTimeout(() => {
+        dragItem.style.display = "none";
+    }, 0)
+})
+
+dragItem.addEventListener("dragend", () => {
+    dragItem.style.display = "block";
+})
+//element.style.display = "inline-block";
