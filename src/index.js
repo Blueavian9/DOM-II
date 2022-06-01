26d1612c6b4a7aaed47e537d6f863301564baafa
cesar.aguilar <Blueavian9@gmail.com>
2022-06-01 01:29:17 -0700

    solution

--- src/index.js
+++ src/index.js
@@ -21,7 +21,40 @@ window.onload = function (evt) {
   document.body.addEventListener('click', evt => {
     evt.target.classList.toggle('mirror')     
   }) 
+
+  // 4- dbclick
+  document.body.addEventListener('dblclick', evt => {
+    evt.target.innerHTML = '' 
+  })
+
+  // 5- keydown
+  window.addEventListener('keydown', evt => {
+    if (evt.key === 6) {
+        document.body.innerHTML = 'YOU RAN ORDER 66'
+    }
+  })
+
+  // 6- mousemove
+  document.body.addEventListener('mousemove', evt => {
+    const { clientX, clientY } = evt
+    console.log(`mouse is at ${clientX}, ${clientY}`)
+  })
+
+  // 7- mouseeneter
+  // 8- mouseleave
+  const destinations = document.querySelectorAll('.destination')
+  for (let destination of destinations) {
+    destination.addEventListener('mouseenter', () => {
+      destination.style.fontWeight = 'bold'
+    })
+    destination.addEventListener('mouseleave', () => {
+      setTimeout(() => {  
+        destination.style.fontWeight = 'initial'
+      }, 500)   
+    })
+  }
 }
+
 // assigning a variable to the nav-link
 
 // mouse over = blue)
